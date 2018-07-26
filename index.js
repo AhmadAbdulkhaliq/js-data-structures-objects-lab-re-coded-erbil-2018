285b1e1b1d2af975fe6971b991c74172ac677d92
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-07-26 21:33:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ let driver ={};
 function updateDriverWithKeyAndValue(object,key,value){
  return Object.assign({},object ,{[key]:value});
 }
+
+function destructivelyUpdateDriverWithKeyAndValue(object , key,value){
+  [key]=value;
+}
